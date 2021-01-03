2ef4d297615a1f71f754b7a1bbd126f745437191
ksemenza <ksemenza13@gmail.com>
2021-01-03 13:55:20 -0500

    update ReadMe

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Home Page Container</h1>
+      <div className='contact-wrap'>
+        <div className='test tool-test' data-tooltip={'781-866-1073'}>
+        <div className='test tool-test' data-tooltip={'ksemenza.247@gmail.com'}>
+            <div className='test tool-test' data-tooltip={'Kim Semenza'}>
+              <p className='test hover-test'>About</p>
+              </div>
+          
+        </div>
+        </div>
+        </div>
+        
+
+      <div className='envelope' data-tooltip='Project Title' >
+        <div className='hidden-content'>
+          <h3>Projects</h3>
+          <p>This Is A test Just a test</p>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = props => {
+
+
+    return (
+        <div>
+            <div className='contact-info'>
+                
+            </div>
+            <h1>Contact Info</h1>
+        </div>
+    )
+}
